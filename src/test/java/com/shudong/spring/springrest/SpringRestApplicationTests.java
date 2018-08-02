fef528d5e1c39349f8f21edf0f30a58fa383e4f9
shudong <zhangshudong@gmail.com>
2018-08-03 00:40:29 +0800

    Initialise spring boot project

---
+++ src/test/java/com/shudong/spring/springrest/SpringRestApplicationTests.java
@@ -0,0 +1,18 @@
+package com.shudong.spring.springrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class SpringRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
