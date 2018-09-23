2c99b2e04fe38c505a8559c0d5be49013551ca46
shudong <zhangshudong@gmail.com>
2018-09-23 16:18:10 +0800

    Add CommandLineRunner

--- src/main/java/com/shudong/spring/springrest/SpringRestApplication.java
+++ src/main/java/com/shudong/spring/springrest/SpringRestApplication.java
@@ -1,16 +1,33 @@
 package com.shudong.spring.springrest;
 
+import com.shudong.spring.springrest.repository.UserEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableJpaRepositories(considerNestedRepositories = true)
 @EnableFeignClients
+@Slf4j
 public class SpringRestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringRestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(UserEntity.Repo repo) {
+        return args -> {
+            log.info("CommandLineRunner running in the SpringRestApplication class...");
+            log.info("args = {}", Arrays.toString(args));
+            log.info("repo = {}", repo.toString());
+        };
+    }
+
 }
